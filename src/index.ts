4b286b27c2c03561f5b6130286f33c1c6dc53657
JUNoym <junnyaooyama@gmail.com>
2021-06-22 12:32:49 +0900

    ⚡️ ESlint Prretier

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import World from './world'
 
-const root = document.getElementById("root");
-const world = new World("反映されるかテスト");
+const root = document.getElementById('root')
+const world = new World('反映されるかテスト')
 
 world.sayHello(root)

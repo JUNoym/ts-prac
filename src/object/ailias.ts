cc20e1bf6b15c599e522fb74ba29daa379e4e9d9
JUNoym <junnyaooyama@gmail.com>
2021-06-25 11:44:29 +0900

    ✨ オブジェクト型定義　型エイリアスなど

---
+++ src/object/ailias.ts
@@ -0,0 +1,80 @@
+import { logmessage } from "function/parms";
+
+export default function ailiasSample() {
+    //　型エイリアス　同じ型を何度も定義する必要がなくなるので便利
+    type Country = {
+        capital: string,
+        population: number,
+        name: string,
+        language: string
+    }
+
+    const Japan: Country = {
+        capital: 'Tokyo',
+        population: 130000000,
+        name: 'Japan',
+        language: 'japanese'
+    }
+    console.log(Japan);
+
+    const america: Country = {
+        capital: 'washington D.C.',
+        population: 300000000,
+        name: 'America',
+        language: 'English'
+    }
+    console.log(america);
+
+
+    //合併型(union)と交差型(intersection)を理解する
+    type Knight = {
+        hp: number,
+        sp: number,
+        weapon: string,
+        swordSkill: string
+    }
+
+    type Wizard = {
+        hp: number,
+        mp: number,
+        weapon: string,
+        magicSkill: string
+    }
+
+    //合併型　騎士か魔法使いのどちらかに成長していく
+    type Adventurer = Knight | Wizard
+
+    // 交差型　騎士と魔法使いの特製両方を併せ持つ強い冒険者
+    type Paladin = Knight & Wizard
+
+    // Knightよりの冒険者
+    const adventurer1: Adventurer = {
+        hp: 100,
+        sp: 30,
+        weapon: '檜のぼう',
+        swordSkill: '五月雨ぎり'
+    }
+
+    // Wizardよりの冒険者
+    const adventurer2: Adventurer = {
+        hp: 80,
+        mp: 30,
+        weapon: '檜の杖',
+        magicSkill: 'メラ',
+    }
+
+    console.log(adventurer1);
+    console.log(adventurer2);
+
+    const paladin: Paladin = {
+        hp: 300,
+        sp: 100, //Knight と　Wizardにある型がかけてしまうとエラーが出る
+        mp: 200,
+        weapon: '伝説の剣',
+        swordSkill: 'ギガスラッシュ',
+        magicSkill: 'メラゾーマ'
+    }
+
+    console.log(paladin);
+
+}
---
+++ src/object/object.ts
@@ -0,0 +1,58 @@
+export default function objectSample() {
+    const a: object = {
+        name: "Junya",
+        age: 23
+    }
+    // a.name //エラーが出る
+
+    // オブジェクトリテラル記法で型定義をする
+    let country: {
+        langeage: string,
+        name: string
+    } = {
+        langeage: 'Japanese',
+        name: 'Japan'
+    }
+    console.log('Sample1', country)
+
+    country = {
+        langeage: 'English',
+        name: 'America'
+    }
+
+    console.log('sample2', country)
+
+    //オプショナルとreadonly
+    let Junya: {
+        age: number,
+        lastName: string,
+        readonly firstName: string,
+        gender?: string
+    } = {
+        age: 23,
+        lastName: 'Oyama',
+        firstName: 'Junya',
+    }
+
+    console.log('sampel4', Junya);
+
+    Junya.gender = 'male'
+    console.log(Junya);
+    Junya.lastName = '佐藤'
+    // Junya.firstName = 'tanjiro'　//read-onlyなので上書きできない
+    console.log(Junya);
+
+    // インデックスシグネチャ
+    const capitals: {
+        [countryName: string]: string
+    } = {
+        America: 'Wachington',
+        Japan: 'Tokyo',
+        canada: 'Otawa'
+    }
+
+    console.log(capitals);
+
+
+}
+
